abfe2444e7193c88048b3b827660881cc54de312
GustavodePaulaGorges <gustavodep.gorges@gmail.com>
2023-03-08 16:50:37 -0300

    addition added

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,13 +1,22 @@
 import { Component } from "react";
-import { Text, Button, StyleSheet, View } from 'react-native';
+import { Text, Button, StyleSheet, View, TextInput } from 'react-native';
 
 export default class Counter extends Component {
     state = {
-        count: 0,
+        count: 1,
+        step: 1,
+        valor1: 0,
+        valor2: 0,
+        result: 0,
     }
     incrementar() {
         this.setState({
-            count: this.state.count + 1
+            count: this.state.count + this.state.step
+        })
+    }
+    adicionarValores(){
+        this.setState({
+            result: this.state.valor1 + this.state.valor2
         })
     }
     dobrar() {
@@ -35,10 +44,41 @@ export default class Counter extends Component {
 
         }
     }
+    alteraStep(text){
+        this.setState({ step: parseInt(text)})
+    }
+    val1(text){
+        this.setState({ valor1: parseInt(text)})
+    }
+    val2(text){
+        this.setState({ valor2: parseInt(text)})
+    }
     render() {
         return (
             <View style={styles.contador}>
-                <Text>Contador: {this.state.count}</Text>
+
+                <Text>Valor 1: </Text>
+                <TextInput
+                    value={this.state.valor1.toString()}
+                    onChange={(e) => {this.val1(e.nativeEvent.text)}}
+                />
+                <Text>Valor 2: </Text>
+                <TextInput  
+                    value={this.state.valor2.toString()}
+                    onChange={(e) => {this.val2(e.nativeEvent.text)}}
+                />
+
+                <Text>Step: </Text>
+                <TextInput
+                    value={this.state.step.toString()}
+                    onChange={(e) => {this.alteraStep(e.nativeEvent.text)}}
+                />
+
+                
+                <Button onPress={() => this.adicionarValores()} title="=" />
+                
+                <Text>Resultado: {this.state.result} |</Text>
+                <Text>Contador: {this.state.count} </Text>
                 <Button onPress={() => this.incrementar()} title="+" />
                 <Button onPress={() => this.reduzir()} title="-" />
                 <Button onPress={() => this.dobrar()} title="2x" />
